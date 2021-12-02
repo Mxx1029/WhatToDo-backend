54f3b89a085e947e03a69c083c10ba8de9c017ae
Mxx1029 <lia.pim@icloud.com>
2021-12-02 15:43:38 +0100

    issue#8-user-login 1st faulty version that created JSON parse error

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,55 +1,69 @@
+import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
 // Get a list of all users
 const getUsers = (req, res) => {
-    
-    // just for testing
-    res.json({ success: "ok" })
+	// just for testing
+	res.json({ success: "ok" });
 };
 
 const getUser = () => {};
 
 // Add/register a new user
-const addUser = async (req, res) => {
-    try {
-        // Check if user already exists
-        const userCheck = await User.findOne({ email: req.body.email });
-        if (userCheck) {
-            console.log(">> Error while registering user: Email already exists");
-            return res.status(400).json({ error: "Email already exists" });
-            // OR (instead of line above, but keep the console.log()):
-            // const e = new Error("Email already exists");
-            // e.status(400);
-            // next(e);
-            // return;
-        }
+const addUser = async (req, res, next) => {
+	try {
+		// Check if user already exists
+		const userCheck = await User.findOne({ email: req.body.email });
+		if (userCheck) {
+			const err = new Error("Email already exists");
+			err.status = 400;
+			next(err);
+			return;
+		}
 
-        // Add new user to the database
-        const user = await User.register(req.body);
-        console.log(`>> New user ${user._id}, username: "${user.name}" saved to database.`);
-        // just to test the hashing
-        // console.log(user.password);
-        res.status(201);
-        res.json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-        });
-    } catch (error) {
-        // To make sure no email address is published into the log
-        if (error.message.indexOf("email") !== -1) {
-            console.log(">> Error while registering user (email)");
-            return res.status(400).json({ error: "Check inputs" });
-        }
-        console.log(">> Error while registering user: ", error.message);
-        res.status(400).json({ error: "Check inputs" });
-        // OR (instead of line above, but keep the console.log()):
-        // next(error);
-    }
+		// Add new user to the database
+		const user = await User.register(req.body);
+		console.log(
+			`>> New user ${user._id}, username: "${user.name}" saved to database.`
+		);
+		// just to test the hashing
+		// console.log(user.password);
+		res.status(201);
+		res.json({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+		});
+	} catch (error) {
+		// To make sure no email address is published into the log
+		if (error.message.indexOf("email") !== -1) {
+			console.log(">> Error while registering user (email)");
+			return res.status(400).json({ error: "Check inputs" });
+		}
+		next(error);
+	}
 };
 
 // Log in a registered user
-const loginUser = () => {};
+const loginUser = async (req, res, next) => {
+	try {
+		const user = await User.login(req.body);
+		if (!user) {
+			const err = new Error("User not found. Register first.");
+			err.status = 400;
+			next(err);
+			return;
+		}
+
+		// Create JWT token
+		const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+        
+
+		res.json({ user, token });
+	} catch (error) {
+        next(error);
+    }
+};
 
 // Update a user
 const updateUser = () => {};
--- libs/crypto.js
+++ libs/crypto.js
@@ -1,11 +1,29 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
-async function hash(password) {
-    return await bcrypt.hash(password, 10);
-};
+/**
+ * This function hashes the input string
+ *
+ * @param {string} password - to be hashed
+ * @param {number} - number of salt rounds to make the hash more secure
+ * @returns {Promise<string>} - the hashed password
+ */
+function hash(password) {
+	return bcrypt.hash(password, 10);
+}
 
-async function compareHashes(incomingPassword, savedHash) {
-    return await bcrypt.compare(incomingPassword, savedHash);
-};
+/**
+ * This function compares an incoming password to a hash saved in the database
+ *
+ * @param {string} incomingPassword - comes from login req.body
+ * @param {string} savedHash - saved hash of that user
+ * @returns {Promise<boolean>} true, if incoming password matches saved hash
+ */
+
+function compareHashes(incomingPassword, savedHash) {
+	if (!incomingPassword) {
+		return false;
+	}
+	return bcrypt.compare(incomingPassword, savedHash);
+}
 
 export { hash, compareHashes };
